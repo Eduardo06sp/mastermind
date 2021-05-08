2acbaa7083ce8a8c8c597a28708bcf7ec1b7724e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-08 14:00:31 -0700

    Add introduction to initiate game

---
+++ main.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+def intro
+  code_pegs = [1, 2, 3, 4, 5, 6]
+
+  puts 'Welcome!'
+  puts 'You may type in lower case throughout the game.'
+  puts 'Would you like to be the codemaster or the codebreaker?'
+
+  input = gets.chomp.downcase
+  until %w[codemaster codebreaker].include?(input)
+    puts 'Please choose "codemaster" or "codebreaker."'
+    input = gets.chomp.downcase
+  end
+
+  new_board = DecodingBoard.new
+
+  if input == 'codemaster'
+    codemaster = HumanCodemaster.new
+    codebreaker = ComputerCodebreaker.new
+  else
+    codemaster = ComputerCodemaster.new
+    codebreaker = HumanCodebreaker.new
+  end
+
+  pattern = codemaster.create_pattern(code_pegs)
+  new_game = Mastermind.new(new_board, code_pegs, pattern, codemaster, codebreaker)
+  new_game.play_rounds
+end
+
+intro
