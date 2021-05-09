db7c5ad7bbb04b52ea93b9259584f0b8f6c34876
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-08 22:32:55 -0700

    Add feedback method to generate key pegs each round

--- human_codemaster.rb
+++ human_codemaster.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HumanCodemaster
+  attr_accessor :key_pegs
+
   def initialize
     @key_pegs = { black: 0, white: 0 }
   end
@@ -35,5 +37,41 @@ class HumanCodemaster
 
       color_location && color_location + 1
     end
+  end
+
+  def give_feedback(guess)
+    reset_key_pegs
+
+    guess_remainder = guess
+    pattern.each_with_index do |pattern_slot, index|
+      if pattern_slot == guess_remainder[index]
+        guess_remainder[index] = nil
+        key_pegs[:black] += 1
+      end
+    end
+
+    pattern_remainder = pattern.map.with_index do |pattern_slot, i|
+      if guess_remainder[i].nil?
+        nil
+      else
+        pattern_slot
+      end
+    end
+
+    pattern_remainder.each do |pattern_slot|
+      next if pattern_slot.nil?
+      next unless guess_remainder.include?(pattern_slot)
+
+      matching_color = guess_remainder.find_index(pattern_slot)
+      guess_remainder[matching_color] = nil
+      key_pegs[:white] += 1
+    end
+
+    key_pegs
+  end
+
+  def reset_key_pegs
+    key_pegs[:black] = 0
+    key_pegs[:white] = 0
   end
 end
