abe6f09ec37bc5790beef51d74d1213981ffe746
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 10:03:22 -0700

    Display board after each guess

--- mastermind.rb
+++ mastermind.rb
@@ -27,6 +27,7 @@ class Mastermind
     feedback = codemaster.give_feedback(guess, pattern)
 
     board.add_round(display_round(feedback, guess, current_round))
+    board.display_board
     self.current_round += 1
   end
 end
