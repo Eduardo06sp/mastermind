5b221e61081ed0b79edacfddb1bf7be649f608f9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 02:11:14 -0700

    Add missing argument and create getter method

--- mastermind.rb
+++ mastermind.rb
@@ -5,7 +5,7 @@ require_relative 'game_interface'
 class Mastermind
   include GameInterface
 
-  attr_reader :codemaster, :codebreaker, :max_rounds
+  attr_reader :pattern, :codemaster, :codebreaker, :max_rounds
   attr_accessor :current_round
 
   def initialize(board, code_pegs, code_peg_colors, pattern, codemaster, codebreaker)
@@ -24,7 +24,7 @@ class Mastermind
     end_game('codemaker') if current_round >= max_rounds
 
     guess = codebreaker.make_guess(codemaster, current_round)
-    feedback = codemaster.give_feedback(guess)
+    feedback = codemaster.give_feedback(guess, pattern)
 
     board.add_round(display_round(feedback, guess, current_round))
     self.current_round += 1
