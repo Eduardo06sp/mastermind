99969552dc29fa1a18bb46e0d0494229ecc61097
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 12:51:31 -0700

    Add ComputerCodemaster class that inherits from HumanCodemaster

---
+++ computer_codemaster.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class ComputerCodemaster < HumanCodemaster
+  def create_pattern(code_pegs, _colors)
+    pattern = []
+
+    4.times do
+      random_code_peg = code_pegs.sample
+      pattern.push(random_code_peg)
+    end
+
+    pattern
+  end
+end
