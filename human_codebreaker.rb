62e3405097a3281dcd20257318b196acca2a569a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 22:37:02 -0700

    Explain HumanCodebreaker class

--- human_codebreaker.rb
+++ human_codebreaker.rb
@@ -2,6 +2,11 @@
 
 require_relative 'human_codemaster'
 
+# HumanCodebreaker uses methods from HumanCodemaster
+# so it inherits from the aforementioned
+#
+# The main difference is that a (human) codebreaker needs to input guesses,
+# while a codemaster does not
 class HumanCodebreaker < HumanCodemaster
   def make_guess(code_pegs, code_peg_colors)
     puts 'Make a guess!'
