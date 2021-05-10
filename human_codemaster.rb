99f369de76b6624c567b7ad880d8ff5b671f73d9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 22:53:40 -0700

    Refactor; Make methods private except create_pattern & give_feedback

--- human_codemaster.rb
+++ human_codemaster.rb
@@ -21,36 +21,6 @@ class HumanCodemaster
     validate_pattern(input, code_pegs, code_peg_colors)
   end
 
-  def display_valid_code_pegs(colors)
-    valid_code_pegs = ''
-    colors.each { |color| valid_code_pegs += " #{color}" }
-    puts "Valid code pegs:#{valid_code_pegs}"
-  end
-
-  def validate_pattern(input, code_pegs, code_peg_colors)
-    input_array = input.split('')
-    input_to_num = color_to_num(input_array, code_peg_colors)
-
-    until input_array.all? { |color| code_peg_colors.include?(color) } &&
-          input_to_num.length == 4 &&
-          input_to_num.all? { |num| code_pegs.include?(num) }
-      puts 'Please enter a valid pattern.'
-      input = gets.chomp.upcase
-      input_array = input.split('')
-      input_to_num = color_to_num(input_array, code_peg_colors)
-    end
-
-    input_to_num
-  end
-
-  def color_to_num(colors, code_peg_colors)
-    colors.map do |color|
-      color_location = code_peg_colors.find_index(color)
-
-      color_location && color_location + 1
-    end
-  end
-
   def give_feedback(guess, pattern)
     reset_key_pegs
 
@@ -80,6 +50,38 @@ class HumanCodemaster
     end
 
     key_pegs
+  end
+
+  private
+
+  def display_valid_code_pegs(colors)
+    valid_code_pegs = ''
+    colors.each { |color| valid_code_pegs += " #{color}" }
+    puts "Valid code pegs:#{valid_code_pegs}"
+  end
+
+  def validate_pattern(input, code_pegs, code_peg_colors)
+    input_array = input.split('')
+    input_to_num = color_to_num(input_array, code_peg_colors)
+
+    until input_array.all? { |color| code_peg_colors.include?(color) } &&
+          input_to_num.length == 4 &&
+          input_to_num.all? { |num| code_pegs.include?(num) }
+      puts 'Please enter a valid pattern.'
+      input = gets.chomp.upcase
+      input_array = input.split('')
+      input_to_num = color_to_num(input_array, code_peg_colors)
+    end
+
+    input_to_num
+  end
+
+  def color_to_num(colors, code_peg_colors)
+    colors.map do |color|
+      color_location = code_peg_colors.find_index(color)
+
+      color_location && color_location + 1
+    end
   end
 
   def reset_key_pegs
