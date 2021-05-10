933b9e10d780c11fc3169c00855fa5c5c51adf12
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-09 22:33:39 -0700

    Refactor; Use single quotes instead of double quotes

--- human_codebreaker.rb
+++ human_codebreaker.rb
@@ -4,7 +4,7 @@ require_relative 'human_codemaster'
 
 class HumanCodebreaker < HumanCodemaster
   def make_guess(code_pegs, code_peg_colors)
-    puts "Make a guess!"
+    puts 'Make a guess!'
     display_valid_code_pegs(code_pegs, code_peg_colors)
     input = gets.chomp.upcase
     validate_pattern(input, code_pegs, code_peg_colors)
